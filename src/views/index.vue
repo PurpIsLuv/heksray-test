193963005e86887eaad50fa61075da49a3fdaba3
purpisluv <purpisluv@gmail.com>
2021-06-11 11:08:00 +0700

    add search by title and text

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="index">
     <v-row>
-      <v-text-field placeholder="Поиск" outlined clearable></v-text-field>
+      <v-text-field
+        placeholder="Поиск"
+        outlined
+        clearable
+        v-model="search"
+      ></v-text-field>
       <v-btn fab dark color="success" class="ml-2">
         <v-icon>mdi-plus</v-icon>
       </v-btn>
@@ -14,17 +19,27 @@
       </v-chip-group>
     </v-row>
     <v-row wrap>
-      <v-card v-for="card in cards" :key="card.id" width="400px" class="ma-1">
-        <v-card-title>{{ card.title }}</v-card-title>
+      <v-card
+        v-for="article in filtredArticleList"
+        :key="article.id"
+        width="400px"
+        class="ma-1"
+      >
+        <v-card-title>{{ article.title }}</v-card-title>
         <v-card-subtitle class="px-2">
-          <v-chip v-for="(tag, index) in card.tags" :key="index" class="ma-1">
+          <v-chip
+            v-for="(tag, index) in article.tags"
+            :key="index"
+            class="ma-1"
+          >
             {{ tag }}
           </v-chip>
         </v-card-subtitle>
-        <v-card-text>{{ card.text }}</v-card-text>
+        <v-card-text class="article__text">{{ article.text }}</v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn color="primary">В топ</v-btn>
+          <v-btn color="success">В топ</v-btn>
+          <v-btn color="primary">Изменить</v-btn>
           <v-btn color="error">Удалить</v-btn>
         </v-card-actions>
       </v-card>
@@ -33,31 +48,31 @@
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 export default {
   data() {
     return {
       selectedTags: [],
       tags: ['All', 'top', 'new', 'funny'],
-      cards: [
-        {
-          id: 1,
-          title: 'Title',
-          tags: ['All', 'top'],
-          text: 'Greyhound divisively hello coldly wonderfully marginally far upon excluding.'
-        },
-        {
-          id: 2,
-          title: 'Title',
-          tags: ['All', 'top'],
-          text: 'Greyhound divisively hello coldly wonderfully marginally far upon excluding.'
-        },
-        {
-          id: 3,
-          title: 'Title',
-          tags: ['All', 'top'],
-          text: 'Greyhound divisively hello coldly wonderfully marginally far upon excluding.'
-        }
-      ]
+      search: ''
+    }
+  },
+  computed: {
+    ...mapState({
+      articleList: (state) => state.article.list
+    }),
+    filtredArticleList() {
+      if (this.search) {
+        return this.articleList.filter((article) => {
+          const search = this.search.toLowerCase()
+          const text = article.text.toLowerCase()
+          const title = article.title.toLowerCase()
+          return text.includes(search) || title.includes(search)
+        })
+      } else {
+        return this.articleList
+      }
     }
   }
 }
@@ -66,5 +81,12 @@ export default {
 <style lang="scss">
 .index {
   padding: 20px 0;
+}
+
+.article {
+  &__text {
+    height: 150px;
+    overflow-y: auto;
+  }
 }
 </style>
